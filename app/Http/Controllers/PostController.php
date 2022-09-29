c236f9eb67038c4970497bd3789e389ba023233d
sana ullah <sanaullah1696@gmail.com>
2022-09-29 14:36:15 +0500

    initial work

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+class PostController extends Controller
+{
+     public function index()
+    {
+        return view('blog');
+    }
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        return redirect('blog')->with('status', 'Blog Form Data Has Been inserted');
+    }
+}
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form handling in laravel 8</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container mt-4">
+  @if(session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+  @endif
+  <div class="card">
+    <div class="card-header text-center font-weight-bold">
+      Example FROM
+    </div>
+    <div class="card-body">
+      <form name="blog" id="add-blog-post-form" method="post" action="{{url('store-form')}}">
+       @csrf
+        <div class="form-group">
+          <label for="exampleInputEmail1">Title</label>
+          <input type="text" id="title" name="title" class="form-control" required="">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Description</label>
+          <textarea name="description" class="form-control" required=""></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>  
+</body>
+</html>
